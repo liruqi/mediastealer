50b612ccbc5424ae8bcdc467262cad0d12c69942
liruqi@gmail.com <liruqi@gmail.com@ff73ba00-4769-11de-8855-c187e5f49513>
2011-03-26 11:52:02 +0000

    remove octet-stream from default rule, to reduce miss-match

--- defaults/preferences/options.js
+++ defaults/preferences/options.js
@@ -3,4 +3,4 @@ pref("extensions.stealer.enabled", true);
 pref("extensions.stealer.showStatusbar", true);
 pref("extensions.stealer.useCache", true);
 pref("extensions.stealer.alwaysConfirm", true);
-pref("extensions.stealer.rulesJSON", "[{\"rtype\":\"1\",\"enabled\":\"true\",\"des\":\"Video\",\"url\":\"http:\\\\/\\\\/\",\"ct\":\"video|flv|f4v|mp4|octet-stream\",\"dir\":\"\"},{\"rtype\":\"2\",\"enabled\":\"false\",\"des\":\"Audio\",\"url\":\"http:\\\\/\\\\/.+(\\\\.mp3|\\\\.wma)\",\"ct\":\"mpeg|mp3|wma|ogg|.*\",\"dir\":\"\"},{\"rtype\":\"3\",\"enabled\":\"false\",\"des\":\"Flash\",\"url\":\"http:\\\\/\\\\/\",\"ct\":\"swf|flash|shockwave\",\"dir\":\"\"}]");
+pref("extensions.stealer.rulesJSON", "[{\"rtype\":\"1\",\"enabled\":\"true\",\"des\":\"Video\",\"url\":\"http:\\\\/\\\\/\",\"ct\":\"video|flv|f4v|mp4\",\"dir\":\"\"},{\"rtype\":\"2\",\"enabled\":\"false\",\"des\":\"Audio\",\"url\":\"http:\\\\/\\\\/.+(\\\\.mp3|\\\\.wma)\",\"ct\":\"mpeg|mp3|wma|ogg|.*\",\"dir\":\"\"},{\"rtype\":\"3\",\"enabled\":\"false\",\"des\":\"Flash\",\"url\":\"http:\\\\/\\\\/\",\"ct\":\"swf|flash|shockwave\",\"dir\":\"\"}]");
