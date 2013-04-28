b863ec485229d34c08ba99f058bae0283311d35d
alex.rosseel@hotmail.com <alex.rosseel@hotmail.com@ff73ba00-4769-11de-8855-c187e5f49513>
2013-04-28 20:15:36 +0000

    Modified downloadall procedure for FF20

--- chrome/content/stealer.js
+++ chrome/content/stealer.js
@@ -1511,6 +1511,19 @@ onDownloadAll: function() {
               var obj_File_Target = IOservice.newFileURI(file2);
               var isPrivate = true;
               var dl = downloadManager.addDownload(downloadManager.DOWNLOAD_TYPE_DOWNLOAD, obj_URI_Source, obj_File_Target, '', null, Math.round(Date.now() * 1000), null, persist, isPrivate);
+              var appinfo = Components.classes["@mozilla.org/xre/app-info;1"].getService(Components.interfaces.nsIXULAppInfo);
+		      var versionchecker = Components.classes["@mozilla.org/xpcom/version-comparator;1"].getService(Components.interfaces.nsIVersionComparator);
+              if (versionchecker.compare(appinfo.version, 20) >=0) {
+              var persistListener = new StealerDownloader2(Stealer, task);
+              downloadManager.addPrivacyAwareListener(persistListener);
+              persist.progressListener = dl;
+              var privacyContext = null;
+              persist.saveURI(dl.source, null, null, null, null, dl.targetFile, privacyContext);
+              task.curr = 0;
+              task.DownloadID = dl.guid; 
+              task.stat = "Transferring";
+              }
+              else {
               var persistListener = new StealerDownloader(Stealer, task);
               downloadManager.addListener(persistListener);
               persist.progressListener = dl;
@@ -1519,8 +1532,9 @@ onDownloadAll: function() {
 
               task.curr = 0;
               task.DownloadID = dl.id;
+              task.stat = "Transferring";
+              } 
               treeitem.firstChild.childNodes[0].setAttribute("DownloadID", task.DownloadID);
-              task.stat = "Transferring"; 
               }
               else if (stat == "Paused")
               {
